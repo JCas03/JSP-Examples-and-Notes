89ef0266252b4da15ca243f239a6b3a635d5d30e
Juan Castorena <03juan.c.castorena@gmail.com>
2022-11-30 11:15:40 -0600

    Adding Examples and Notes

---
+++ JSTL/src/main/java/Model/Employee.java
@@ -0,0 +1,17 @@
+package Model;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Employee {
+
+        private int id;
+        private String name;
+        private String role;
+
+
+    }
+
